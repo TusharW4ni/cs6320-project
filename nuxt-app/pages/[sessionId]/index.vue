482abcdca9d741c5879a6d735e9b3ada38d945f9
maunikaa <m.achanta77@gmail.com>
2025-04-19 20:18:00 -0500

    displayed ingredients list

--- nuxt-app/pages/[sessionId]/index.vue
+++ nuxt-app/pages/[sessionId]/index.vue
@@ -5,22 +5,33 @@ const sessionId = route.params.sessionId;
 const { data } = await useFetch(`/api/session/get/${sessionId}`);
 
 console.log({ session: data.value.session });
-console.log({ length: data.value?.session.Messages.length });
+//console.log({ length: data.value?.session.Messages.length });
+console.log({ ingredients: data?.value?.session?.Recipe.Ingredients });
+console.log({ ingredients: data?.value?.session?.Recipe.Ingredients.length });
 </script>
+
 <template>
   <Navbar />
-  <!-- Messages -->
   <div></div>
-  <!-- Substitutions -->
-  <div v-if="!data.value?.session.Messages" class="text-white">
+  <div v-if="data?.session?.Recipe?.Ingredients?.length > 0" class="text-white">
     <div class="w-full h-[calc(100vh-4rem)] flex justify-center items-center">
       <div>
         <ul>
-          <input type="checkbox" />
-          <div>afa</div>
-          <div>asdfasd</div>
+          <li
+            v-for="ingredient in data.session.Recipe.Ingredients"
+            :key="ingredient.id"
+            class="flex items-center"
+          >
+            <input type="checkbox" class="mr-2" />
+            <div>{{ ingredient.text }}</div>
+          </li>
         </ul>
       </div>
+    </div>
+  </div>
+  <div v-else class="text-white">
+    <div class="w-full h-[calc(100vh-4rem)] flex justify-center items-center">
+      <div>No ingredients found for this recipe.</div>
     </div>
   </div>
 </template>
